8eb9ab01ef1a5c65634c4f3191abd20c08e2aa32
emirhanyener <yeneremirhann@gmail.com>
2022-07-25 23:45:16 +0300

    add util test

---
+++ Test/TestMain.java
@@ -0,0 +1,11 @@
+package Test;
+
+public class TestMain {
+    TestMain(){
+        new UtilTest();
+    }
+
+    public static void main(String[] args) {
+        new TestMain();
+	}
+}
---
+++ Test/UtilTest.java
@@ -0,0 +1,37 @@
+package Test;
+import Util.*;
+
+public class UtilTest {
+    public UtilTest(){
+        int all = 3;
+        int success = 0;
+
+        if(positionTest()) success++;
+        if(velocityTest()) success++;
+        if(sizeTest()) success++;
+
+        System.out.println("Util tests " + success + " / " + all);
+    }
+
+    public boolean positionTest(){
+        Position p = new Position();
+        if(p.getX() == 0 && p.getY() == 0){
+            return true;
+        }
+        return false;
+    }
+    public boolean velocityTest(){
+        Velocity v = new Velocity();
+        if(v.getX() == 0 && v.getY() == 0){
+            return true;
+        }
+        return false;
+    }
+    public boolean sizeTest(){
+        Size s = new Size(100, 100);
+        if(s.getHeight() == 100 && s.getWidth() == 100){
+            return true;
+        }
+        return false;
+    }
+}
