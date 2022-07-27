6e809e4c9833f262f1aaab43547f9a63b7ae9dd1
emirhanyener <yeneremirhann@gmail.com>
2022-07-27 23:19:31 +0300

    add setting class

---
+++ sglib/Setting.java
@@ -0,0 +1,13 @@
+package sglib;
+
+import java.awt.Color;
+
+public class Setting {
+	public static final int WINDOW_POSITION_X = 50;
+	public static final int WINDOW_POSITION_Y = 50;
+	public static final int WINDOW_WIDTH = 800;
+	public static final int WINDOW_HEIGHT = 600;
+	public static final long REFRESH_RATE = 10;
+	public static final float GRAVITY = 9.81f;
+	public static final Color BACKGROUND_COLOR = new Color(255, 255, 255);
+}
