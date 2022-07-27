5d08cfefe406a5585a3e88338e203575de4d774c
emirhanyener <yeneremirhann@gmail.com>
2022-07-27 23:25:12 +0300

    add gameobjects class

---
+++ sglib/GameObjects.java
@@ -0,0 +1,33 @@
+package sglib;
+
+import sglib.Util.Object;
+import java.util.LinkedList;
+
+public class GameObjects {
+	private static GameObjects instance;
+	private LinkedList<Object> objects = null;
+    
+	public static GameObjects getInstance() {
+		if(instance == null) 
+			instance = new GameObjects();
+            
+		return instance;
+	}
+
+	public void addObject(Object item) {
+		objects.add(item);
+	}
+	public void removeObject(Object item) {
+		objects.remove(item);
+	}
+	public Object findObject(String name) {
+		for(int i = 0; i < objects.size(); i++) 
+			if(objects.get(i).getName() == name)
+				return objects.get(i);
+
+		return null;
+	}
+	public LinkedList<Object> getObjects(){
+		return objects;
+	}
+}
