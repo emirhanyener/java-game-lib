fc92336b1ca7d3df0a8f902e2dacd90c0a8802a5
emirhanyener <yeneremirhann@gmail.com>
2022-07-29 23:37:07 +0300

    add default color to setting

--- sglib/Setting.java
+++ sglib/Setting.java
@@ -10,4 +10,5 @@ public class Setting {
 	public static final long REFRESH_RATE = 10;
 	public static final float GRAVITY = 9.81f;
 	public static final Color BACKGROUND_COLOR = new Color(255, 255, 255);
+	public static final Color DEFAULT_COLOR = new Color(255, 0, 255);
 }
