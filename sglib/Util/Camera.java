25b68ff047d9b27a9988dc0a05490fb04540b9e8
emirhanyener <yeneremirhann@gmail.com>
2022-07-30 17:18:30 +0300

    update camera

--- sglib/Util/Camera.java
+++ sglib/Util/Camera.java
@@ -1,26 +1,5 @@
 package sglib.Util;
 
-import sglib.Setting;
-
-public class Camera {
-	protected Position position;
-	
-	public Camera(Position position) {
-		this.position = position;
-	}
-	public Camera() {
-		this.position = new Position();
-	}
-	
-	public Position getPosition() {
-		return new Position(position.getX() - Setting.WINDOW_WIDTH / 2, position.getY() - Setting.WINDOW_HEIGHT / 2);
-	}
-	
-	public void setPosition(Position position) {
-		this.position = position;
-	}
-	
-	public Position getRawPosition() {
-		return this.position;
-	}
+public interface Camera {
+	public Position getPosition();
 }
