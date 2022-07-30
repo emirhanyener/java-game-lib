02a70ac6d7e6b444f65818324b8a32b48277639e
emirhanyener <yeneremirhann@gmail.com>
2022-07-30 23:11:22 +0300

    add shortcuts to object

--- sglib/Util/Object.java
+++ sglib/Util/Object.java
@@ -2,6 +2,8 @@ package sglib.Util;
 
 import java.awt.Image;
 
+import sglib.GameObjects;
+
 public class Object{
 	protected String name;
 	public Position position;
@@ -35,6 +37,29 @@ public class Object{
 
 	public void setImage(Image image) {
 		this.image = image;
+	}
+
+	public static Object create(String name, Position position, Size size){
+		Object object = new Object(name, position, size);
+		Object.add(object);
+		return object;
+	}
+	public static Object create(String name){
+		Object object = new Object(name);
+		Object.add(object);
+		return object;
+	}
+	public static Object find(String name){
+		return GameObjects.getInstance().findObject(name);
+	}
+	public static void add(Object object){
+		GameObjects.getInstance().addObject(object);
+	}
+
+	public Object setFunction(EventFunction function){
+		function.setObject(this);
+		GameObjects.getInstance().addEvent(function);
+		return this;
 	}
 
 	public void OnTriggered(Object object) { };
