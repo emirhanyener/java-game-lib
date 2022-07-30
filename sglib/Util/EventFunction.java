bb76d1793839ef79a37bc5bde902121f7c9ef8c6
emirhanyener <yeneremirhann@gmail.com>
2022-07-30 23:10:27 +0300

    update event function

--- sglib/Util/EventFunction.java
+++ sglib/Util/EventFunction.java
@@ -3,8 +3,17 @@ package sglib.Util;
 import sglib.GameObjects;
 
 public abstract class EventFunction {
+    private Object object;
+
     public EventFunction(){
         GameObjects.getInstance().addEvent(this);
+    }
+
+    public void setObject(Object object) {
+        this.object = object;
+    }
+    public Object getObject() {
+        return this.object;
     }
 
     public abstract void execute();
