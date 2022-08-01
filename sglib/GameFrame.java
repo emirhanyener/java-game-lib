2351d6ed89d1de43fe0ce3a0bd499bf54a9f68e7
emirhanyener <yeneremirhann@gmail.com>
2022-08-01 18:43:52 +0300

    move update method to sglib

--- sglib/GameFrame.java
+++ sglib/GameFrame.java
@@ -1,38 +1,18 @@
 package sglib;
 
 import java.awt.BorderLayout;
-import java.util.concurrent.TimeUnit;
 import javax.swing.JFrame;
 
-import sglib.Util.*;
-
 public class GameFrame{
-    GameCanvas canvas;
+    public GameCanvas canvas;
+    public JFrame frame;
     public GameFrame(){
-        JFrame frame = new JFrame();
+        frame = new JFrame();
         frame.setBounds(Setting.WINDOW_POSITION_X, Setting.WINDOW_POSITION_Y, Setting.WINDOW_WIDTH, Setting.WINDOW_HEIGHT);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         canvas = new GameCanvas();
         frame.add(canvas, BorderLayout.CENTER);
         frame.setVisible(true);
-        update();
     }
 
-    public void update() {
-      while(true) {
-        try {
-          TimeUnit.MILLISECONDS.sleep(Setting.REFRESH_RATE);
-        } 
-        catch (InterruptedException e) {
-          e.printStackTrace();
-        }
-        canvas.repaint();
-        for (GameObject item : GameObjects.getInstance().getObjects()) {
-          item.calculate();
-        }
-        for (EventFunction item : GameObjects.getInstance().getEvents()) {
-          item.execute();
-        }
-      }
-    }
 }
--- sglib/SGLib.java
+++ sglib/SGLib.java
@@ -1,17 +1,47 @@
 package sglib;
 
-import sglib.Util.Camera;;
+import sglib.Util.*;
+import java.awt.event.KeyListener;
+import java.util.concurrent.TimeUnit;
 
 public class SGLib {
+	GameFrame gframe;
     public SGLib(){
+		gframe = new GameFrame();
         
     }
 
 	public void start(){
-		GameFrame gframe = new GameFrame();
+		update();
 	}
 
 	public void setMainCamera(Camera mainCamera) {
 		GameObjects.getInstance().setMainCamera(mainCamera);
 	}
+
+	public void setKeyListener(KeyListener kl){
+		gframe.frame.addKeyListener(kl);
+	}
+
+	
+    public void update() {
+		while(true) {
+		  try {
+			TimeUnit.MILLISECONDS.sleep(Setting.REFRESH_RATE);
+		  } 
+		  catch (InterruptedException e) {
+			e.printStackTrace();
+		  }
+		  gframe.canvas.repaint();
+		  for (GameObject item : GameObjects.getInstance().getObjects()) {
+			item.calculate();
+		  }
+		  for (Physics item : GameObjects.getInstance().getPhysics()) {
+			item.calculate();
+		  }
+		  for (EventFunction item : GameObjects.getInstance().getEvents()) {
+			item.execute();
+		  }
+		}
+	  }
 }
