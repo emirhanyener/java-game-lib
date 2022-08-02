42c279593b59db2c1eb355c708ab50e86fccf648
emirhanyener <yeneremirhann@gmail.com>
2022-08-02 23:10:23 +0300

    add camera zoom

---
+++ sglib/Camera/Camera.java
@@ -0,0 +1,20 @@
+package sglib.Camera;
+
+import sglib.Util.Position;
+
+public abstract class Camera {
+	private float zoomValue;
+
+	public Camera(){
+		this.zoomValue = 1;
+	}
+	
+	public float getZoomValue(){
+		return this.zoomValue;
+	}
+	public void setZoomValue(float zoomValue){
+		this.zoomValue = zoomValue;
+	}
+
+	public abstract Position getPosition();
+}
