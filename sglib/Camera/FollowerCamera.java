f4c180b89a51085c0f620edf39fc7764323ae19e
emirhanyener <yeneremirhann@gmail.com>
2022-08-04 23:35:23 +0300

    update follower camera

--- sglib/Camera/FollowerCamera.java
+++ sglib/Camera/FollowerCamera.java
@@ -21,6 +21,17 @@ public class FollowerCamera extends Camera{
     }
 
     @Override
+    public Position getCameraPosition() {
+
+        return target.position;
+    }
+
+    @Override
+    public void setPosition(Position position) {
+        
+    }
+
+    @Override
     public Position getPosition() {
         return new Position(target.position.getX() + offset.getX() + (target.size.getWidth() / 2) - Setting.WINDOW_WIDTH / 2, target.position.getY() + offset.getY() + target.size.getHeight() - Setting.WINDOW_HEIGHT / 2);
     }
