bed245195f579c78dc7c4ecec6b50fca8e1ab0f1
emirhanyener <yeneremirhann@gmail.com>
2022-08-04 23:41:35 +0300

    add new camera tests

--- Test/CameraTest.java
+++ Test/CameraTest.java
@@ -1,16 +1,21 @@
 package Test;
 
+import sglib.GameObjects;
 import sglib.Camera.*;
+import sglib.Util.GameObject;
 import sglib.Util.Position;
+import sglib.Util.Size;
 
 public class CameraTest {
     public CameraTest(){
-        int all = 2;
+        int all = 4;
         int success = 0;
 
         System.out.println("Camera tests start");
         if(staticCameraDefaultConstructorTest()) success++;
         if(staticCameraParameterConstructorTest()) success++;
+        if(followerCameraPositionTest()) success++;
+        if(followerCameraVariableObjectPositionTest()) success++;
         System.out.println("Camera tests end");
 
         System.out.println("Camera tests " + success + " / " + all);
@@ -30,6 +35,30 @@ public class CameraTest {
             return true;
         }
         System.out.println("staticCameraParameterConstructorTest failed");
+        return false;
+    }
+    public boolean followerCameraPositionTest(){
+        GameObjects.getInstance().resetInstance();
+        GameObject.create("o1", new Position(150, 300), new Size(50, 50));
+        GameObject.create("o2", new Position(0, 10), new Size(50, 50));
+        FollowerCamera c = new FollowerCamera(GameObject.find("o1"));
+        if(c.getCameraPosition().getX() == 150 && c.getCameraPosition().getY() == 300){
+            return true;
+        }
+        System.out.println("followerCameraPositionTest failed");
+        return false;
+    }
+    public boolean followerCameraVariableObjectPositionTest(){
+        GameObjects.getInstance().resetInstance();
+        GameObject.create("o1", new Position(150, 300), new Size(50, 50));
+        GameObject.create("o2", new Position(300, 600), new Size(50, 50));
+        GameObject.create("o3", new Position(600, 1200), new Size(50, 50));
+        GameObject.find("o2").position.setX(1000);
+        FollowerCamera c = new FollowerCamera(GameObject.find("o2"));
+        if(c.getCameraPosition().getX() == 1000 && c.getCameraPosition().getY() == 600){
+            return true;
+        }
+        System.out.println("followerCameraVariableObjectPositionTest failed");
         return false;
     }
 }
