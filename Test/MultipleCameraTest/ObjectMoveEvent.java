c97e921fe19a0135f93b1d3af7fdb9b3e42c14c2
emirhanyener <yeneremirhann@gmail.com>
2022-08-05 23:25:37 +0300

    add multiple camera test

---
+++ Test/MultipleCameraTest/ObjectMoveEvent.java
@@ -0,0 +1,11 @@
+package Test.MultipleCameraTest;
+
+import sglib.Util.*;
+
+public class ObjectMoveEvent extends EventFunction{
+    @Override
+    public void execute() {
+        this.getObject().velocity.setX(1);
+        this.getObject().velocity.setY(0.5f);
+    }
+}
