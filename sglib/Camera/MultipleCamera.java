7f2351453d4a530de73d592ae8f266cd3c9e149e
emirhanyener <yeneremirhann@gmail.com>
2022-08-05 23:23:56 +0300

    add multiple camera class

---
+++ sglib/Camera/MultipleCamera.java
@@ -0,0 +1,52 @@
+package sglib.Camera;
+
+import sglib.Setting;
+import sglib.Util.GameObject;
+import sglib.Util.Position;
+
+//follower camera follows a target object automaticly
+public class MultipleCamera extends Camera{
+    private GameObject[] targets;
+    private Position offset;
+
+    //constuctors
+    public MultipleCamera(GameObject[] objects, Position offset){
+        this.targets = objects;
+        this.offset = offset;
+    }
+    //set offset position 0, 0 if offset not sended
+    public MultipleCamera(GameObject[] objects){
+        this.targets = objects;
+        this.offset = new Position();
+    }
+
+    @Override
+    public Position getCameraPosition() {
+        float sumX = 0;
+        float sumY = 0;
+        for (GameObject gameObject : targets) {
+            sumX += gameObject.position.getX();
+            sumY += gameObject.position.getY();
+        }
+        this.position.setX(sumX / targets.length);
+        this.position.setY(sumY / targets.length);
+        return this.position;
+    }
+
+    @Override
+    public void setPosition(Position position) {
+        
+    }
+
+    @Override
+    public Position getPosition() {
+        float sumX = 0;
+        float sumY = 0;
+        for (GameObject gameObject : targets) {
+            sumX += gameObject.position.getX();
+            sumY += gameObject.position.getY();
+        }
+
+        return new Position((sumX / targets.length) + offset.getX() - Setting.WINDOW_WIDTH / 2, (sumY / targets.length) + offset.getY() - Setting.WINDOW_HEIGHT / 2);
+    }
+}
