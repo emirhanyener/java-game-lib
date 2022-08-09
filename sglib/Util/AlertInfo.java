cde9a557b0bdc98ac7a68a5e4aa8e6577808bc45
emirhanyener <yeneremirhann@gmail.com>
2022-08-09 23:13:16 +0300

    add alert info class

---
+++ sglib/Util/AlertInfo.java
@@ -0,0 +1,34 @@
+package sglib.Util;
+
+import sglib.Util.StaticValues.AlertLevel;
+
+public class AlertInfo {
+    private String message;
+    private AlertLevel alertLevel;
+
+    //constructors
+    public AlertInfo(String message){
+        this.message = message;
+        this.alertLevel = AlertLevel.info;
+    }
+    public AlertInfo(String message, AlertLevel level){
+        this.message = message;
+        this.alertLevel = level;
+    }
+
+    //getters
+    public AlertLevel getAlertLevel() {
+        return alertLevel;
+    }
+    public String getMessage() {
+        return message;
+    }
+
+    //setters
+    public void setAlertLevel(AlertLevel alertLevel) {
+        this.alertLevel = alertLevel;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
---
+++ sglib/Util/StaticValues.java
@@ -0,0 +1,9 @@
+package sglib.Util;
+
+public class StaticValues {
+    public static enum AlertLevel{
+        info,
+        warning,
+        error
+    }
+}
