b81a212f5de207e9494f57e9d9acc2f3b91ab98e
emirhanyener <yeneremirhann@gmail.com>
2022-08-18 13:08:28 +0300

    update gui objects

--- sglib/GUI/GUIBox.java
+++ sglib/GUI/GUIBox.java
@@ -1,14 +1,31 @@
 package sglib.GUI;
 
+import java.awt.Color;
+import java.awt.Graphics;
+
 import sglib.Util.Size;
 
 public class GUIBox extends GUI{
     private Size size;
+
+    public GUIBox(Size size, Color color){
+        this.size = size;
+        this.setColor(color);
+    }
+    public GUIBox(Size size){
+        this.size = size;
+    }
 
     public Size getSize() {
         return size;
     }
     public void setSize(Size size) {
         this.size = size;
+    }
+
+    @Override
+    public void draw(Graphics g) {
+        g.setColor(this.getColor());
+        g.fillRect((int)this.getPosition().getX(), (int)this.getPosition().getY(), this.size.getWidth(), this.size.getHeight());
     }
 }
--- sglib/GUI/GUIText.java
+++ sglib/GUI/GUIText.java
@@ -1,5 +1,39 @@
 package sglib.GUI;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+
 public class GUIText extends GUI{
+    private String text;
+    private int textSize = 12;
+
+    public GUIText(String text, Color color){
+        this.text = text;
+        this.setColor(color);
+    }
+    public GUIText(String text){
+        this.text = text;
+        this.setColor(Color.BLACK);
+    }
+
+    public String getText() {
+        return text;
+    }
+    public int getTextSize() {
+        return textSize;
+    }
+    public void setText(String text) {
+        this.text = text;
+    }
+    public void setTextSize(int textSize) {
+        this.textSize = textSize;
+    }
     
+    @Override
+    public void draw(Graphics g) {
+        g.setColor(this.getColor());
+        g.setFont(new Font("Arial", 0, this.getTextSize()));
+        g.drawString(text, (int)this.getPosition().getX(), (int)this.getPosition().getY());
+    }
 }
