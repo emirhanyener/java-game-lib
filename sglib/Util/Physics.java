ae60300f746fd56c5cf0c212434399936d309999
emirhanyener <yeneremirhann@gmail.com>
2022-08-19 17:16:24 +0300

    update physics

--- sglib/Util/Physics.java
+++ sglib/Util/Physics.java
@@ -1,11 +1,11 @@
 package sglib.Util;
 
+import java.util.LinkedList;
+
 import sglib.*;
 
 public class Physics {
     private GameObject object;
-	private boolean isMovable = true;
-	private boolean isGravityControlled = false;
 	
     //set controlled object
 	public Physics(GameObject object) {
@@ -15,58 +15,21 @@ public class Physics {
     //calculate selected object
 	public void calculate() {
         //loop all object
-        for(int o = 0; o < GameObjects.getInstance().getObjects().size(); o++) {
-            GameObject item = GameObjects.getInstance().getObjects().get(o);
+        LinkedList<GameObject> detectedObjectsRight = object.checkTrigger(0, 0, (object.size.getWidth() / 2) + object.velocity.getX(), 0, 3, Setting.VELOCITY_GUI_ACTIVE, "right");
+        LinkedList<GameObject> detectedObjectsLeft = object.checkTrigger(0, 0, (-1) * (object.size.getWidth() / 2) + object.velocity.getX(), 0, 3, Setting.VELOCITY_GUI_ACTIVE, "left");
+        LinkedList<GameObject> detectedObjectsDown = object.checkTrigger(0, 0, 0, (object.size.getHeight() / 2) + object.velocity.getY(), 3, Setting.VELOCITY_GUI_ACTIVE, "down");
+        GameObject.staticGUIId = 0;
 
-            //skip iteration if item and object is same
-            if(item == object){
-                continue;
-            }
-            
-            //conflict control for y axis
-            if(object.position.getX() + object.size.getWidth() > item.position.getX() && object.position.getX() < item.position.getX() + item.size.getWidth() && object.position.getY() + object.velocity.getY() + object.size.getHeight() > item.position.getY() && object.position.getY() + object.velocity.getY() < item.position.getY() + item.size.getHeight())
-            {	
-                if(!item.isAbstract()) {
-                    if(object.position.getY() + object.velocity.getY() < item.position.getY()) {
-                        object.position.setY(item.position.getY() - object.size.getHeight());
-                    }
-                    object.velocity.setY(0);
-                }
-            }
-            else {
-                isGravityControlled = true;
-            }
-            
-            //confict control for x axis
-            if(object.position.getY() + object.size.getHeight() - 1 > item.position.getY()){
-                if(object.position.getY() < item.position.getY() + item.size.getHeight()) {
-                    if(object.position.getX() + object.size.getWidth() + object.velocity.getX() > item.position.getX()) {
-                        if(object.position.getX() + object.velocity.getX() < item.position.getX() + item.size.getWidth()) {
-                            if(item.isAbstract()) {	
-                                item.OnTriggered(item);
-                                continue;
-                            }
-                            else{
-                                isMovable = false;
-                        
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        
-        if(isMovable) {
+        if(detectedObjectsRight.size() != 0 || detectedObjectsLeft.size() != 0) {
             //move if object movable
-            object.position.setX(object.position.getX() + object.velocity.getX());
+            object.velocity.setX(0);
         }
-        if(isGravityControlled) {
+        if(detectedObjectsDown.size() == 0) {
             //add gravity force to velocity y
             object.velocity.addY(Setting.GRAVITY * 6);
         }
-
-        //reset variables
-        isMovable = true;
-        isGravityControlled = false;
+        else{
+            object.position.setY(detectedObjectsDown.get(0).position.getY() - object.size.getHeight());
+        }
     }
 }
