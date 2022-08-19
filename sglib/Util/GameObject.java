718203cc7a09e29d89ad476e71dc5e4ffcb57f81
emirhanyener <yeneremirhann@gmail.com>
2022-08-19 17:13:54 +0300

    add gameobject check trigger

--- sglib/Util/GameObject.java
+++ sglib/Util/GameObject.java
@@ -1,8 +1,11 @@
 package sglib.Util;
 
+import java.awt.Color;
 import java.awt.Image;
+import java.util.LinkedList;
 
 import sglib.GameObjects;
+import sglib.GUI.*;
 
 public class GameObject{
 	protected String name;
@@ -11,6 +14,7 @@ public class GameObject{
 	public Size size;
 	public boolean isAbstract = false;
 	private Image image;
+	public static int staticGUIId = 0;
 
 	//constructors
 	public GameObject(String name, Position position, Size size, Image image) {
@@ -84,6 +88,42 @@ public class GameObject{
 	public void calculate(){
 		this.position.addX(this.velocity.getX());
 		this.position.addY(this.velocity.getY());
+	}
+
+	public LinkedList<GameObject> checkTrigger(float offsetX, float offsetY, float x, float y, int step, boolean isVisible, String guiName){
+		if(isVisible){
+			if(GameObjects.getInstance().findGUIObject(guiName) == null){
+				GameObjects.getInstance().addGUIObject(new GUILine(new Position(this.position.getX() + offsetX, this.position.getY() + offsetY), new Position(this.position.getX() + offsetX + x, this.position.getY() + offsetY + y)).setName(guiName).setColor(Color.GREEN));
+			}
+			GameObjects.getInstance().findGUIObject(guiName).getPosition().setX(this.position.getX() + offsetX + this.size.getWidth() / 2);
+			GameObjects.getInstance().findGUIObject(guiName).getPosition().setY(this.position.getY() + offsetY + this.size.getHeight() / 2);
+			GameObjects.getInstance().findGUIObject(guiName).toGUILine().getToPosition().setX(this.position.getX() + offsetX + x + this.size.getWidth() / 2);
+			GameObjects.getInstance().findGUIObject(guiName).toGUILine().getToPosition().setY(this.position.getY() + offsetY + y + this.size.getHeight() / 2);
+		}
+
+		LinkedList<GameObject> detectedObjects = new LinkedList<GameObject>();
+
+		for(int i = 0; i < step + 2; i++){
+			for(GameObject item : GameObjects.getInstance().getObjects()){
+				if(item == this){
+					continue;
+				}
+				if(this.position.getX() + (this.size.getWidth() / 2) + offsetX + (x / step) * i > item.position.getX()){
+					if(this.position.getX() + (this.size.getWidth() / 2) + offsetX + (x / step) * i < item.position.getX() + item.size.getWidth()){
+						if(this.position.getY() + (this.size.getHeight() / 2) + offsetY + (y / step) * i > item.position.getY()){
+							if(this.position.getY() + (this.size.getHeight() / 2) + offsetY + (y / step) * i < item.position.getY() + item.size.getHeight()){
+								detectedObjects.add(item);
+							}
+						}
+					}
+				}
+			}
+		}
+		return detectedObjects;
+	}
+
+	public void destroy(){
+		GameObject.remove(this.getName());
 	}
 
 	//execute triggered function if any object in object
