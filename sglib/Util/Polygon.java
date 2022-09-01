3deda9c6a054f109917514d42f531cfd7d54c1f8
emirhanyener <yeneremirhann@gmail.com>
2022-09-01 16:52:46 +0300

    update polygon draw

--- sglib/Util/Polygon.java
+++ sglib/Util/Polygon.java
@@ -2,19 +2,32 @@ package sglib.Util;
 
 import java.awt.Graphics;
 
+import sglib.GameObjects;
+
 public class Polygon {
     private Polygon(){
 
     }
 
-    public static void draw(Graphics g, Position position, Size size, int shape, int rotation){
-		int[] xPositions = new int[shape];
-		int[] yPositions = new int[shape];
+    public static void draw(Graphics g, GameObject item/* , Position anchor, int shape, int angle*/){
+		int[] xPositions = new int[4];
+		int[] yPositions = new int[4];
 
+		/*
 		for(int i = 0; i < shape; i++){
-			xPositions[i] = (int)(position.getX() + size.getWidth() * Math.cos(Math.toRadians(((180 - ((shape - 2) * 180) / shape) * i ) + rotation)));
-			yPositions[i] = (int)(position.getY() + size.getHeight() * Math.sin(Math.toRadians(((180 - ((shape - 2) * 180) / shape) * i ) + rotation)));
+			xPositions[i] = (int)(position.getX() + (size.getWidth()) * Math.cos(Math.toRadians(((180 - ((shape - 2) * 180) / shape) * i ) + 45)));
+			yPositions[i] = (int)(position.getY() + (size.getHeight()) * Math.sin(Math.toRadians(((180 - ((shape - 2) * 180) / shape) * i ) + 45)));
 		}
-		g.fillPolygon(xPositions, yPositions, shape);
+		*/
+		xPositions[0] = (int)item.position.getX() - item.size.getWidth() / 2 - (int)GameObjects.getInstance().getMainCamera().getPosition().getX();
+		yPositions[0] = (int)item.position.getY() - item.size.getHeight() / 2 - (int)GameObjects.getInstance().getMainCamera().getPosition().getY();
+		xPositions[1] = (int)item.position.getX() + item.size.getWidth() / 2 - (int)GameObjects.getInstance().getMainCamera().getPosition().getX();
+		yPositions[1] = (int)item.position.getY() - item.size.getHeight() / 2 - (int)GameObjects.getInstance().getMainCamera().getPosition().getY();
+		xPositions[2] = (int)item.position.getX() + item.size.getWidth() / 2 - (int)GameObjects.getInstance().getMainCamera().getPosition().getX();
+		yPositions[2] = (int)item.position.getY() + item.size.getHeight() / 2 - (int)GameObjects.getInstance().getMainCamera().getPosition().getY();
+		xPositions[3] = (int)item.position.getX() - item.size.getWidth() / 2 - (int)GameObjects.getInstance().getMainCamera().getPosition().getX();
+		yPositions[3] = (int)item.position.getY() + item.size.getHeight() / 2 - (int)GameObjects.getInstance().getMainCamera().getPosition().getY();
+
+		g.fillPolygon(xPositions, yPositions, 4);
     }
 }
