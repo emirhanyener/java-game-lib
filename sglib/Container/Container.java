c35df795b87274acbafdc27ce4c85cf9e9bfeb41
emirhanyener <yeneremirhann@gmail.com>
2022-09-14 21:59:22 +0300

    add container class

---
+++ sglib/Container/Container.java
@@ -0,0 +1,21 @@
+package sglib.Container;
+
+import java.util.LinkedList;
+
+public abstract class Container <T>{
+    LinkedList<T> objects = new LinkedList<T>();
+
+    public Container(){
+        container();
+        createAll();
+    }
+
+    public abstract void container();
+    protected abstract void createAll();
+    public void addContainer(T object){
+        this.objects.add(object);
+    }
+    public LinkedList<T> getAll(){
+        return objects;
+    }
+}
