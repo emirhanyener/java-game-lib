60ae113e6da6e09e1541b4ffb0361ea5c80eb341
emirhanyener <yeneremirhann@gmail.com>
2022-09-29 11:38:14 +0300

    update multiple camera example

--- Examples/MultipleCamera/ObjectMoveEvent.java
+++ Examples/MultipleCamera/ObjectMoveEvent.java
@@ -7,7 +7,13 @@ import sglib.*;
 public class ObjectMoveEvent extends EventFunction {
     @Override
     public void execute() {
-        this.getObject().position.addX(1);
-        this.getObject().position.addY(0.5f);
+        if(KeyManager.getManager().isKeyNumMinus()){
+            this.getObject().position.addX(1);
+            this.getObject().position.addY(0.5f);
+        }
+        if(KeyManager.getManager().isKeyNumAdd()){
+            this.getObject().position.addX(-1);
+            this.getObject().position.addY(-0.5f);
+        }
     }
 }
