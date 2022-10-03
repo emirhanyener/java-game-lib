a6d07c96d98734260dc8ddc0c833b0f9321464e5
emirhanyener <yeneremirhann@gmail.com>
2022-10-03 10:08:56 +0300

    update gui example

--- Examples/GUI/Game.java
+++ Examples/GUI/Game.java
@@ -14,6 +14,8 @@ public class Game {
 
         GameObjects.getInstance().addGUIObject((new GUIBox(new Size(100, 100))).setPosition(new Position(100, 100)).setName("box1"));
         GameObjects.getInstance().addGUIObject((new GUIText("text")).setPosition(new Position(0, 400)).setName("text1"));
+        GameObjects.getInstance().addGUIObject((new GUILine(new Position(0, 0), new Position(100, 100))));
+
         GameObject.create("empty");
         GameObject.find("empty").setFunction(new ClickEvent());
 
