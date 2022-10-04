e8d37f8efa78ff2aecfc777255a0d46ae5bd6b2e
emirhanyener <yeneremirhann@gmail.com>
2022-10-04 18:11:42 +0300

    add gameobject button

--- sglib/Developer/DeveloperFrame.java
+++ sglib/Developer/DeveloperFrame.java
@@ -8,19 +8,33 @@ import javax.swing.*;
 
 public class DeveloperFrame extends JFrame{
     private static DeveloperFrame frame = null;
+    private static GameObjectsFrame gameObjectsFrame = null;
 
     private DeveloperFrame(){
         this.setTitle("Developer Console");
         this.setBounds(10, 10, 300, 600);
         this.setLayout(new FlowLayout());
-        JButton shutdown = new JButton("shutdown");
-        shutdown.addActionListener(new ActionListener(){
+        gameObjectsFrame = new GameObjectsFrame();
+        gameObjectsFrame.setVisible(false);
+
+        JButton shutdownBtn = new JButton("shutdown");
+        shutdownBtn.addActionListener(new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent e) {
                 System.exit(0);
             }
         });
-        this.add(shutdown);
+        this.add(shutdownBtn);
+
+        JButton gameObjectsBtn = new JButton("GameObjects");
+        gameObjectsBtn.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                gameObjectsFrame.setVisible(true);
+            }
+        });
+        this.add(gameObjectsBtn);
+
     }
 
     public static DeveloperFrame getInstance(){
