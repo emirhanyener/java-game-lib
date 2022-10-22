2dda20076f8b8b7a053431492454d8c5c9f8231d
emirhanyener <yeneremirhann@gmail.com>
2022-10-22 20:35:42 +0300

    update setting align

--- sglib/Setting.java
+++ sglib/Setting.java
@@ -4,23 +4,23 @@ import java.awt.Color;
 
 public class Setting {
 	// window settings
-	public static final int WINDOW_POSITION_X = 50;
-	public static final int WINDOW_POSITION_Y = 50;
-	public static final int WINDOW_WIDTH = 800;
-	public static final int WINDOW_HEIGHT = 600;
-	public static final boolean MAXIMIZED = false;
+	public static final int     WINDOW_POSITION_X   = 50;
+	public static final int     WINDOW_POSITION_Y   = 50;
+	public static final int     WINDOW_WIDTH        = 800;
+	public static final int     WINDOW_HEIGHT       = 600;
+	public static final boolean MAXIMIZED           = false;
 	
 	// game settings
-	public static final long REFRESH_RATE = 10;
-	public static final float GRAVITY = 9.81f / 100;
-	public static final Color BACKGROUND_COLOR = new Color(180, 240, 255);
-	public static final Color DEFAULT_COLOR = new Color(255, 0, 255);
-	public static final boolean IS_ALERT_ACTIVE = true;
-	public static final int MAX_ALERT_NUMBER = 3;
-	public static final boolean IS_GUI_ACTIVE = true;
-	public static final int STOKE_WIDTH = 2;
+	public static final long    REFRESH_RATE        = 10;
+	public static final float   GRAVITY             = 9.81f / 100;
+	public static final Color   BACKGROUND_COLOR    = new Color(180, 240, 255);
+	public static final Color   DEFAULT_COLOR       = new Color(255, 0, 255);
+	public static final boolean IS_ALERT_ACTIVE     = true;
+	public static final int     MAX_ALERT_NUMBER    = 3;
+	public static final boolean IS_GUI_ACTIVE       = true;
+	public static final int     STOKE_WIDTH         = 2;
 	public static final boolean VELOCITY_GUI_ACTIVE = true;
-	public static final boolean IS_KEY_REPEAT = false;
-	public static final int KEY_REPEAT_DELAY = 10;
-	public static final boolean DEVELOPER_MODE = true;
+	public static final boolean IS_KEY_REPEAT       = false;
+	public static final int     KEY_REPEAT_DELAY    = 10;
+	public static final boolean DEVELOPER_MODE      = true;
 }
