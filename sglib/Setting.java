e4289a5c356b8d2660586c51e3aa47ed42520767
emirhanyener <yeneremirhann@gmail.com>
2022-10-22 20:40:24 +0300

    setting comments

--- sglib/Setting.java
+++ sglib/Setting.java
@@ -11,16 +11,27 @@ public class Setting {
 	public static final boolean MAXIMIZED           = false;
 	
 	// game settings
+	//Refresh rate(miliseconds)
 	public static final long    REFRESH_RATE        = 10;
+	//Physics gravity force power
 	public static final float   GRAVITY             = 9.81f / 100;
+	//Background color(rgb)
 	public static final Color   BACKGROUND_COLOR    = new Color(180, 240, 255);
+	//Default gameobject color(rgb)
 	public static final Color   DEFAULT_COLOR       = new Color(255, 0, 255);
+	//Alerts visible state
 	public static final boolean IS_ALERT_ACTIVE     = true;
+	//If alerts are visible, max showed alert number
 	public static final int     MAX_ALERT_NUMBER    = 3;
+	//GUI objects visible state
 	public static final boolean IS_GUI_ACTIVE       = true;
 	public static final int     STOKE_WIDTH         = 2;
+	//Physics gameobject velocity GUI visible state
 	public static final boolean VELOCITY_GUI_ACTIVE = true;
+	//Key down repeat state
 	public static final boolean IS_KEY_REPEAT       = false;
+	//Key down repeat delay
 	public static final int     KEY_REPEAT_DELAY    = 10;
+	//Game developer mode(all gameobject positions and physics velocity info)
 	public static final boolean DEVELOPER_MODE      = true;
 }
