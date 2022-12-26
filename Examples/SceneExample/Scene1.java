afc268baad08924bc4307cbeb56d5564c5811102
emirhanyener <yeneremirhann@gmail.com>
2022-12-26 22:49:24 +0300

    update scene1

--- Examples/SceneExample/Scene1.java
+++ Examples/SceneExample/Scene1.java
@@ -7,14 +7,14 @@ import sglib.Util.Size;
 public class Scene1 extends Scene{
     @Override
     public void setup() {
-        GameObject.create("obj1", new Position(100, 100), new Size(50, 50));
-        GameObject.create("obj2", new Position(200, 100), new Size(50, 50));
-        GameObject.create("obj3", new Position(200, 200), new Size(50, 50));
-        GameObject.create("obj4", new Position(100, 200), new Size(50, 50));
+        GameObject.create("obj1", new Position(-50, 50), new Size(50, 50));
+        GameObject.create("obj2", new Position(-50, -50), new Size(50, 50));
+        GameObject.create("obj3", new Position(50, 50), new Size(50, 50));
+        GameObject.create("obj4", new Position(50, -50), new Size(50, 50));
 
         GameObject.find("obj1").setColor(100, 20, 20);
-        GameObject.find("obj2").setColor(100, 20, 20);
-        GameObject.find("obj3").setColor(100, 20, 20);
-        GameObject.find("obj4").setColor(100, 20, 20);
+        GameObject.find("obj2").setColor(100, 50, 20);
+        GameObject.find("obj3").setColor(100, 80, 20);
+        GameObject.find("obj4").setColor(100, 110, 20);
     }
 }
