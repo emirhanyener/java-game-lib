746672e9b74e4e983c09e6b65735179c548bd425
emirhanyener <yeneremirhann@gmail.com>
2022-12-26 22:45:22 +0300

    add example scene

---
+++ Examples/SceneExample/Scene1.java
@@ -0,0 +1,20 @@
+package Examples.SceneExample;
+import sglib.Scene.Scene;
+import sglib.Util.GameObject;
+import sglib.Util.Position;
+import sglib.Util.Size;
+
+public class Scene1 extends Scene{
+    @Override
+    public void setup() {
+        GameObject.create("obj1", new Position(100, 100), new Size(50, 50));
+        GameObject.create("obj2", new Position(200, 100), new Size(50, 50));
+        GameObject.create("obj3", new Position(200, 200), new Size(50, 50));
+        GameObject.create("obj4", new Position(100, 200), new Size(50, 50));
+
+        GameObject.find("obj1").setColor(100, 20, 20);
+        GameObject.find("obj2").setColor(100, 20, 20);
+        GameObject.find("obj3").setColor(100, 20, 20);
+        GameObject.find("obj4").setColor(100, 20, 20);
+    }
+}
