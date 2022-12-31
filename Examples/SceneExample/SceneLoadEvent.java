5136e103049c3e3c45bb415cccd590fc3d6c7af2
emirhanyener <yeneremirhann@gmail.com>
2022-12-31 19:05:25 +0300

    add scene load event function

---
+++ Examples/SceneExample/SceneLoadEvent.java
@@ -0,0 +1,22 @@
+package Examples.SceneExample;
+
+import sglib.KeyManager.KeyManager;
+import sglib.Scene.Scene;
+import sglib.Util.Alert;
+import sglib.Util.EventFunction;
+import sglib.Util.StaticValues.AlertLevel;
+
+public class SceneLoadEvent extends EventFunction {
+    @Override
+    public void execute() {
+        Alert.getInstance().addAlert("scene",AlertLevel.info);
+        if (KeyManager.getManager().isKey1()) {
+            Alert.getInstance().addAlert("scene1",AlertLevel.info);
+            Scene.loadScene("Scene1");
+        }
+        if (KeyManager.getManager().isKey2()) {
+            Alert.getInstance().addAlert("scene2",AlertLevel.info);
+            Scene.loadScene("Scene2");
+        }
+    }
+}
