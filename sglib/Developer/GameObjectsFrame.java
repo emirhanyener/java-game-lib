972db2bc8fc12f692015535caf2230d44c9e9b95
emirhanyener <yeneremirhann@gmail.com>
2023-01-01 19:40:04 +0300

    update GameObjectsFrame: dynamic rows

--- sglib/Developer/GameObjectsFrame.java
+++ sglib/Developer/GameObjectsFrame.java
@@ -1,27 +1,28 @@
 package sglib.Developer;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 
 import sglib.GameObjects;
+import sglib.Util.GameObject;
 
 public class GameObjectsFrame extends JFrame implements DeveloperConsole{
     JTable table = new JTable();
     String[][] rows;
+    DefaultTableModel model;
 
     //Added gameobjects info panel
     public GameObjectsFrame(){
         this.setTitle("GameObjects Console");
         this.setBounds(10, 10, 520, 220);
         this.setLayout(null);
-        
-        rows = new String[GameObjects.getInstance().getObjects().size()][3];
-        for(int i = 0; i < GameObjects.getInstance().getObjects().size(); i++){
-            rows[i][0] = GameObjects.getInstance().getObjects().get(i).getName();
-            rows[i][1] = String.valueOf(GameObjects.getInstance().getObjects().get(i).position.getX());
-            rows[i][2] = String.valueOf(GameObjects.getInstance().getObjects().get(i).position.getY());
+        model = (DefaultTableModel)table.getModel();
+        model.addColumn("Name");
+        for (GameObject item : GameObjects.getInstance().getObjects()) {
+            model.addRow(new String[]{item.getName()});
         }
+        table.setModel(model);
 
-        table = new JTable(rows, new String[]{"Name", "X Position", "Y Position"});
         JScrollPane sp = new JScrollPane(table);
         sp.setBounds(0,0,520,185);
 
@@ -31,11 +32,12 @@ public class GameObjectsFrame extends JFrame implements DeveloperConsole{
     //update printed gameobject values
     @Override
     public void update() {
-        for(int i = 0; i < GameObjects.getInstance().getObjects().size(); i++){
-            table.setValueAt(GameObjects.getInstance().getObjects().get(i).getName(), i, 0);
-            table.setValueAt(String.valueOf(GameObjects.getInstance().getObjects().get(i).position.getX()), i, 1);
-            table.setValueAt(String.valueOf(GameObjects.getInstance().getObjects().get(i).position.getY()), i, 2);
-            table.putClientProperty(rootPane, accessibleContext);
+        int counter = model.getRowCount();
+        for (int i = 0; i < counter; i++) {
+            model.removeRow(0);
+        }
+        for (GameObject item : GameObjects.getInstance().getObjects()) {
+            model.addRow(new String[]{item.getName()});
         }
     }
 }
