7c59c887c37e580e73f51b162150bdddc4c4d414
emirhanyener <yeneremirhann@gmail.com>
2023-01-01 19:39:15 +0300

    update scene example game

--- Examples/SceneExample/Game.java
+++ Examples/SceneExample/Game.java
@@ -1,7 +1,12 @@
 package Examples.SceneExample;
 
 import sglib.*;
+import sglib.Camera.StaticCamera;
 import sglib.Scene.Scene;
+import sglib.Util.Alert;
+import sglib.Util.GameObject;
+import sglib.Util.Position;
+import sglib.Util.StaticValues.AlertLevel;
 
 public class Game {
     public Game() {
@@ -10,7 +15,10 @@ public class Game {
 
     public static void main(String[] args) {
         Scene1 scene1 = new Scene1();
+        Scene2 scene2 = new Scene2();
+        GameObjects.getInstance().addGlobalEvent(new SceneLoadEvent());
 
+        GameObjects.getInstance().setMainCamera(new StaticCamera(new Position(0, 0)));
         Scene.loadScene("Scene1");
 
         SGLib sglib = new SGLib();
