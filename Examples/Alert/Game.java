55fc14209efc41932485ebf95471d2f778cbe989
emirhanyener <yeneremirhann@gmail.com>
2023-01-06 22:36:27 +0300

    fix alert example

--- Examples/Alert/Game.java
+++ Examples/Alert/Game.java
@@ -12,9 +12,9 @@ public class Game {
     }
 
     public static void main(String[] args) {
-        GameObject.create("o1", new Position(-500, 500), new Size(1000, 30));
+        GameObject.create("o1", new Position(0, 5000), new Size(1000, 30));
         GameObject.create("player", new Position(0, 0), new Size(30, 30));
-        GameObjects.getInstance().setMainCamera(new FollowerCamera(GameObjects.getInstance().findObject("o2"), new Position(0, 0)));
+        GameObjects.getInstance().setMainCamera(new FollowerCamera(GameObjects.getInstance().findObject("player"), new Position(0, 0)));
 
         GameObject.find("player").addPhysics();
         GameObject.find("player").setFunction(new AlertEvent());
