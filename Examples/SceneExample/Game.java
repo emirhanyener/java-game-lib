7e44b38219e243e14db0c7ad0a6edc93baadb270
emirhanyener <yeneremirhann@gmail.com>
2023-01-06 21:56:13 +0300

    update scene example: add second scene create

--- Examples/SceneExample/Game.java
+++ Examples/SceneExample/Game.java
@@ -6,6 +6,7 @@ import sglib.Scene.Scene;
 import sglib.Util.Alert;
 import sglib.Util.GameObject;
 import sglib.Util.Position;
+import sglib.Util.Size;
 import sglib.Util.StaticValues.AlertLevel;
 
 public class Game {
@@ -14,10 +15,41 @@ public class Game {
     }
 
     public static void main(String[] args) {
-        Scene1 scene1 = new Scene1();
-        Scene2 scene2 = new Scene2();
-        GameObjects.getInstance().addGlobalEvent(new SceneLoadEvent());
+        //first usage
+        new Scene1();
+        new Scene2();
 
+        //second usage
+        new Scene("Scene3") {
+            @Override
+            public void setup() {
+                GameObject.create("obj1", new Position(-50, 50), new Size(50, 50));
+                GameObject.create("obj2", new Position(-50, -50), new Size(50, 50));
+                GameObject.create("obj3", new Position(50, 50), new Size(50, 50));
+                GameObject.create("obj4", new Position(50, -50), new Size(50, 50));
+        
+                GameObject.find("obj1").setColor(20, 20, 20);
+                GameObject.find("obj2").setColor(50, 20, 20);
+                GameObject.find("obj3").setColor(80, 20, 20);
+                GameObject.find("obj4").setColor(110, 20, 20);
+            }
+        };
+        new Scene("Scene4") {
+            @Override
+            public void setup() {
+                GameObject.create("obj1", new Position(-100, 100), new Size(50, 50));
+                GameObject.create("obj2", new Position(-100, -100), new Size(50, 50));
+                GameObject.create("obj3", new Position(100, 100), new Size(50, 50));
+                GameObject.create("obj4", new Position(100, -100), new Size(50, 50));
+
+                GameObject.find("obj1").setColor(20, 20, 20);
+                GameObject.find("obj2").setColor(50, 20, 20);
+                GameObject.find("obj3").setColor(80, 20, 20);
+                GameObject.find("obj4").setColor(110, 20, 20);
+            }
+        };
+
+        GameObjects.getInstance().addGlobalEvent(new SceneLoadEvent());
         GameObjects.getInstance().setMainCamera(new StaticCamera(new Position(0, 0)));
         Scene.loadScene("Scene1");
 
